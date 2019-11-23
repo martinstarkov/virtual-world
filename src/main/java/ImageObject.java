0c1f3c448b5114add724cbd97a36804a76ec2c70
Martin <emailregisterlogin@gmail.com>
2019-11-23 03:26:03 +0000

    ImageObject less coupled with io.File

--- src/main/java/ImageObject.java
+++ src/main/java/ImageObject.java
@@ -1,19 +1,14 @@
 import javafx.scene.image.Image;
 
-import java.io.File;
-
 public class ImageObject {
     private Image javaFxImage;
     private String path;
-    private File file;
-    ImageObject(File file) {
-        this.file = file;
-        this.javaFxImage = new Image(this.file.toURI().toString());
-    }
-    public String getPath() {
-        return path;
+    ImageObject(String path) {
+        this.path = path;
+        this.javaFxImage = new Image(path);
     }
     public Image getJavaFxImage() {
         return javaFxImage;
     }
+    public String getPath() { return path; }
 }
