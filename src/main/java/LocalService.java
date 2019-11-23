4ecb51cd0c321eb8441181cba6fb547825b4d7bc
Martin <emailregisterlogin@gmail.com>
2019-11-23 03:26:14 +0000

    Update LocalService.java

--- src/main/java/LocalService.java
+++ src/main/java/LocalService.java
@@ -19,7 +19,7 @@ public class LocalService implements Service {
         String imageName = id + "-" + direction.toString().toLowerCase();
         File[] matchingFiles = folder.listFiles((dir, name) -> {
             // look for files that start with the sector name and with in an acceptable image format (png / jpg)
-            return name.startsWith(imageName) && (name.endsWith("png") || name.endsWith("jpg"));
+            return name.startsWith(imageName) && (name.endsWith("png") || name.endsWith("PNG") || name.endsWith("JPG") || name.endsWith("jpg"));
         });
             // look for files that start with the sector name and with in an acceptable image format (png / jpg)
         File[] defaultFile = folder.listFiles((dir, name) -> {
@@ -29,13 +29,13 @@ public class LocalService implements Service {
         if (matchingFiles != null) {
             if (matchingFiles.length > 0) {
                 File file = matchingFiles[0].getAbsoluteFile();
-                ImageObject image = new ImageObject(file);
+                ImageObject image = new ImageObject(file.toURI().toString());
                 return image;
             }
         }
         if (defaultFile != null) {
             if (defaultFile.length > 0) {
-                return new ImageObject(defaultFile[0]);
+                return new ImageObject(defaultFile[0].toURI().toString());
             }
         }
         return null;
