77d574ecef4c439c1a28451e37117c0dac8cb8a5
Martin <emailregisterlogin@gmail.com>
2019-11-26 11:51:09 +0000

    Separate some method responsibilities

--- src/main/java/WorldController.java
+++ src/main/java/WorldController.java
@@ -1,56 +1,75 @@
-import javafx.scene.image.Image;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class WorldController {
 
 	private Player player;
-	private Model model;
+	private JSONModel model;
 	private LocalService service;
 	private ViewController viewController;
 
+	// setup MVC components and player
 	public void initialise() {
 
-		player = new Player("Martin");
-		model = new Model(this, player);
+	    // inventoryLimit must be a perfect square, otherwise override to 9 by default
+		player = new Player("Martin", 4);
+		// set custom model here
+		model = new JSONModel(this, player);
 		model.parseFile("sectors.json");
-		service = new LocalService("resources");
-		// model.printMap("entrances");
-	    // image = service.getImage(player.getSector().getName(), 0);
+		// set custom service here
+		service = new LocalService();
 	}
 
-	public void setupViewController(ViewController vc) {
+	// called in the VirtualWorld class when the application start() method has been called
+	public void setup(ViewController vc) {
 		this.viewController = vc;
 		viewController.setWorldController(this);
-		surfaceSelected(player.getSector().getId());
+		// initial setup of interface and inventories
+		surfaceSelected(model.getAreaID());
 		stateChange("");
 	}
 
 	public void surfaceSelected(String id) {
-		model.surfaceChanged(id);
+		model.surfaceUpdate(id);
 	}
-	// called by the model; tells the view to update UI with new button choices corresponding to new area
-	public void surfaceChanged(HashMap<String, String> choices, ArrayList<String> availableSides) {
-		if (player.getSector().getSurface(player.getDirection()) != null) {
-			viewController.updateMenu(player.getContents(), player.getSector().getSurface(player.getDirection()).getContents());
+
+	public void inputInterfaceChange(TreeMap<String, String> choices, ArrayList<String> availableSides) {
+		viewController.updateInputInterface(choices, availableSides);
+	}
+
+	public ImageObject imageChange(String id, String direction) {
+		return service.getImage(id, direction);
+	}
+
+	public void displayChange(String id, String direction) {
+		ImageObject updatedImage = imageChange(id, direction);
+		viewController.updateDisplay(updatedImage);
+	}
+
+	public void pickMenuChange(boolean available, Integer inventoryLimit, HashMap<String, Item> playerContents, HashMap<String, Item> surfaceContents) {
+		if (available) {
+			viewController.updateMenu(inventoryLimit, playerContents, surfaceContents);
 		} else {
-			viewController.updateMenu(player.getContents(), null);
+			viewController.updateMenu(inventoryLimit, playerContents, null);
 		}
-		viewController.updateInterface(choices, availableSides);
-		viewController.updateText(player.getName(), player.getSector().getId(), player.getSector().getDisplayName(), player.getSector().getDisplayText(), player.getDirection());
-		ImageObject newImage = service.getImage(player.getSector().getId(), player.getDirection());
-		viewController.updateDisplay(newImage);
+	}
+
+	public void surfaceChange(TreeMap<String, String> choices, ArrayList<String> availableSides) {
+
+		pickMenuChange(model.surfaceAvailable(), model.getInventoryLimit(), model.getContents("player"), model.getContents(""));
+		inputInterfaceChange(choices, availableSides);
+		displayChange(model.getAreaID(), model.getDirection("true"));
+		viewController.updateText(model.getPlayerName(), model.getAreaID(), model.getAreaName(), model.getAreaText(), model.getDirection("true"));
+
 	}
 
     public void stateChange(String id) {
 		model.updateContainers(id);
     }
 
-	public void inventoryUpdate() {
-		//viewController.updateInventory();
-		viewController.updateMenu(player.getContents(), player.getSector().getSurface(player.getDirection()).getContents());
+	public void inventoryChange() {
+		viewController.updateMenu(model.getInventoryLimit(), model.getContents("player"), model.getContents(""));
 	}
+
 }
