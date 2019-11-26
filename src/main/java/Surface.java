6fb56fbbc5049df884d88d9c7f8588d18127916c
Martin <emailregisterlogin@gmail.com>
2019-11-26 11:52:12 +0000

    Formatting

--- src/main/java/Surface.java
+++ src/main/java/Surface.java
@@ -2,38 +2,35 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Surface implements Inventory {
+
     private String imagePath;
     private Direction direction;
     private ArrayList<Sector> entrances;
     private HashMap<String, Item> contents = new HashMap<String, Item>();
-    //private contents
-    //private entrances
+
     public Surface(Direction direction, String image) {
         this.direction = direction;
         // replace "direction" in "name-direction.format" with for e.g. "north"
         this.imagePath = image.replace("direction", direction.toString().toLowerCase());
     }
-    public String getImagePath() {
-        return imagePath;
-    }
-    public Direction getDirection() {
-        return direction;
-    }
-    public void setEntrances(ArrayList<Sector> entrances) {
-        this.entrances = entrances;
-    }
+
+    public String getImagePath() { return imagePath; }
+    public Direction getDirection() { return direction; }
     public ArrayList<Sector> getEntrances() {
         return entrances;
     }
+
+    public void setEntrances(ArrayList<Sector> entrances) {
+        this.entrances = entrances;
+    }
+
     @Override
     public void addToInventory(Item item) {
         contents.put(item.getId(), item);
     }
 
     @Override
-    public void removeFromInventory(Item item) {
-        contents.remove(item.getId());
-    }
+    public void removeFromInventory(Item item) { contents.remove(item.getId()); }
 
     @Override
     public HashMap<String, Item> getContents() {
