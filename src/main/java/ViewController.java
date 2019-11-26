e073da9f8dffa0b0161a4764db16dfa62235b211
Martin <emailregisterlogin@gmail.com>
2019-11-26 11:51:47 +0000

    Update interface methods

--- src/main/java/ViewController.java
+++ src/main/java/ViewController.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public interface ViewController {
+
     void setWorldController(WorldController worldController);
     void updateDisplay(ImageObject newImage);
-    void updateInterface(HashMap<String, String> choices, ArrayList<String> availableSides);
-    void updateText(String playerName, String areaID, String areaName, String actionText, Direction direction);
-    void updateMenu(HashMap<String,Item> playerInventory, HashMap<String,Item> areaInventory);
+    void updateInputInterface(TreeMap<String, String> choices, ArrayList<String> availableSides);
+    void updateText(String playerName, String areaID, String areaName, String actionText, String direction);
+    void updateMenu(Integer playerInventorySpace, HashMap<String,Item> playerInventory, HashMap<String,Item> areaInventory);
 }
